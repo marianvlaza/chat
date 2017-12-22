5968292f4acb3c759babc825477cb6698bc489d0
Marian <marianvlaza@outlook.com>
2017-12-22 01:34:18 +0100

    Final version - Tried to implement end-to-end test with Protractor

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('chat App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('WELCOME TO THE FRAMEWORK EXAM CHAT SYSTEM');
   });
 });
